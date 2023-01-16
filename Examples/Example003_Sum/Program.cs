9334123130f31318a82a8999a5a6191a16fb51ff
Евгения <jennivertex@gmail.com>
2023-01-16 11:18:12 +0300

    add Example 003 Sum

---
+++ Examples/Example003_Sum/Program.cs
@@ -0,0 +1,15 @@
+﻿int numberA = new Random().Next(1, 10);//1 2 3 4...9
+Console.WriteLine(numberA);
+int nubmerB = new Random().Next(1, 10);//1 2 3 4...9
+Console.WriteLine(nubmerB);
+int result = numberA + nubmerB;
+Console.WriteLine(result);
+
+
+/* или
+int numberA = 31;
+int nubmerB = 5;
+Console.WriteLine(nubmerB);
+int result = numberA + nubmerB;
+Console.WriteLine(result);
+*/
