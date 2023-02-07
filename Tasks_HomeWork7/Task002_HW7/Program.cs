aa2059a021ef7ecc1eebf0106f3e2f9b314f8eda
Евгения <jennivertex@gmail.com>
2023-02-07 17:04:36 +0300

    add Task 002 HW7

---
+++ Tasks_HomeWork7/Task002_HW7/Program.cs
@@ -0,0 +1,84 @@
+﻿/* Задача 50. Напишите программу, которая на вход принимает позиции 
+элемента в двумерном массиве, и возвращает значение этого элемента 
+или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+пользователь вводит индексы 10, 10 - такого элемента нет.
+пользователь вводите индексы 0, 2 - выводим элеменет 7 
+*/
+
+//метод получения числа с консоли
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число. Повторите ввод.");
+        }
+    }
+
+    return result;
+}
+
+//метод, инициализирующий матрицу
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return matrix;
+}
+
+//метод, распечатывающий матрицу
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+int countOfRows = GetNumber("Введите кол-во строк:");
+int countOfColumns = GetNumber("Введите кол-во столбцов:");
+
+int[,] matrix = InitMatrix(countOfRows, countOfColumns);
+
+PrintMatrix(matrix);
+
+
+
+int row = GetNumber("Введите индекс строки: ");
+int colum = GetNumber("Введите индекс столбца: ");
+
+if (row < matrix.GetLength(0) && colum < matrix.GetLength(1))
+    Console.WriteLine($"Значение индексов {row}, {colum} = числу {matrix[row, colum]}");
+else
+    Console.WriteLine($"Введенные индексы за пределами массива{row}, {colum} - такого элемента в массиве нет");
+
+
